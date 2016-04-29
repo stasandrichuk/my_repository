6c4143279a691fbdebd49c772f0ead8e38c6ca32
stasandrichuk <stasandrichuk@users.noreply.github.com>
2016-04-29 21:07:46 +0300

    V1.4-Gmap

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,9 +7,10 @@ class ApplicationController < ActionController::Base
   def set_user
   	User.cuser = current_user
   	@users = User.all
-	@hash = Gmaps4rails.build_markers(@users) do |user, marker|
-	  marker.lat user.latitude
-	  marker.lng user.longitude
-	end
+  	@hash = Gmaps4rails.build_markers(@users) do |user, marker|
+  	  marker.lat user.latitude
+  	  marker.lng user.longitude
+  	end
+    @g_key = "AIzaSyCMl4aZmSFX9mACw1GcwGLUriCcIcj9OZM"
   end
 end
